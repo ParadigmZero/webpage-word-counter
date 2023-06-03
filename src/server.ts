88bca87d9af0f73f2463476ede41c5b8c9d58949
paradigmzero <josephrcalton@gmail.com>
2023-06-03 20:53:36 +0100

    Functionality and tests for word count of a single web page is satisfactory

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,44 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import wordsCounter from 'word-counting'
 
 const app = express();
 
-// Take in the page, we are going to count the words on, in a query parameter named 'site'
-app.get('/', async (req: Request, res: Response) => {
+// Take in the page, we are going to count the words on, in a query parameter named 'page'
+app.get('/', async (req : Request, res : Response) => {
+  // If no query parameter named page is passed in, return a 400 indicating Bad Request
   if(req.query.page === undefined)
   {
-    res.statusCode = 400; // Bad Request
-    res.send('A query parameter named \'page\' must be included in the HTTP GET request, e.g. <API-URL>/?page=http://www.foo.com/bar.html');
+    res.status(400).send('A query parameter named \'page\' must be included in the HTTP GET request, e.g. <API-URL>/?page=http://www.foo.com/bar.html');
     return;
   }
 
+  let body : string;
+
+  // // Add a http:// to the entered url, if it is missing.
+  if((req.query.page as string).substring(0,7).toLowerCase() !== 'http://'
+    &&
+    (req.query.page as string).substring(0,8).toLowerCase() !== 'https://'
+  )
+  {
+
+    req.query.page = 'http://' + req.query.page;
+  }
+
+  // Try fetching the webpage from the URL
   try {
     const response = await fetch(`${req.query.page}`);
     // Convert the response into text
-    const body = await response.text();
-    console.log(body);
-
-    /*
-    Word counting happens here.
-    */
+    body = await response.text();
   } catch (error) {
     console.log("Error fetching page:")
     console.log(error);
-    res.statusCode = 404;
-  }
-
-  if(res.statusCode == 404)
-  {
-    res.send(`Error retrieving page from URL:\n${req.query.page}\ncheck this is a valid url.`);
+    res.status(404).send(`Error retrieving page from URL:\n${req.query.page}\ncheck this is a valid url.`);
     return;
   }
 
-  res.send(req.query.page);
+
+  res.send(wordsCounter(body, { isHtml: true }).wordsCount.toString());
 });
 
 
