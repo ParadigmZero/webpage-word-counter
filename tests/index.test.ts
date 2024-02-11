8bd34ac8f544990eb0026254fdbc6269cfb5339f
Joseph Alton <josephrcalton@gmail.com>
2024-02-11 14:56:04 +0000

    Add failing test to observe actions

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,36 @@
-import {urlResolver, addHTTPtoUrl, getEmbeddedPageUrls, wordCount, dynamicWordCount} from "../src/index";
+import {
+  urlResolver,
+  addHTTPtoUrl,
+  getEmbeddedPageUrls,
+  wordCount,
+  dynamicWordCount,
+} from "../src/index";
 
 test("urls are properly resolved (with base url) in the urlResolver method", () => {
-    expect(urlResolver("http://www.mydomain.com/","./dir/mypage.html")).toEqual("http://www.mydomain.com/dir/mypage.html");
-    expect(urlResolver("http://www.mydomain.com/","mypage.html")).toEqual("http://www.mydomain.com/mypage.html");
-    expect(urlResolver("http://www.mydomain.com/","http://www.adifferentsite.com")).toEqual("http://www.adifferentsite.com");
+  expect(urlResolver("http://www.mydomain.com/", "./dir/mypage.html")).toEqual(
+    "http://www.mydomain.com/dir/mypage.html"
+  );
+  expect(urlResolver("http://www.mydomain.com/", "mypage.html")).toEqual(
+    "http://www.mydomain.com/mypage.html"
+  );
+  expect(
+    urlResolver("http://www.mydomain.com/", "http://www.adifferentsite.com")
+  ).toEqual("http://www.adifferentsite.com");
 });
 
 test("Add http:// to urls where appropriate in addHTTPtoUrl method", () => {
-    expect(addHTTPtoUrl("google.com" as string)).toEqual("http://google.com");
-    expect(addHTTPtoUrl("http://google.com" as string)).toEqual("http://google.com");
-    expect(addHTTPtoUrl("https://google.com" as string)).toEqual("https://google.com");
+  expect(addHTTPtoUrl("google.com" as string)).toEqual("http://google.com");
+  expect(addHTTPtoUrl("http://google.com" as string)).toEqual(
+    "http://google.com"
+  );
+  expect(addHTTPtoUrl("https://google.com" as string)).toEqual(
+    "https://google.com"
+  );
 });
 
 test("The getEmbeddedPageUrls function should extract the url from embedded content tags", () => {
-    expect(getEmbeddedPageUrls(`
+  expect(
+    getEmbeddedPageUrls(`
     <html>
 
 <!-- 
@@ -58,12 +75,20 @@ test("The getEmbeddedPageUrls function should extract the url from embedded cont
 
 </body>
 
-</html>`)).toEqual([ "./index.html","./index.html","./index.html","./index.html",
-    "./index.html","./index.html"]);
+</html>`)
+  ).toEqual([
+    "./index.html",
+    "./index.html",
+    "./index.html",
+    "./index.html",
+    "./index.html",
+    "./index.html",
+  ]);
 });
 
 test("Ignore non-HTML embedded elements", () => {
-    expect(getEmbeddedPageUrls(`
+  expect(
+    getEmbeddedPageUrls(`
     <html>
 <!-- 
     TEST
@@ -92,26 +117,69 @@ test("Ignore non-HTML embedded elements", () => {
     </div>
 </body>
 
-</html>`)).toEqual([]);
+</html>`)
+  ).toEqual([]);
 });
 
 test("The wordCount function should return the correct word count for a given page", () => {
-    expect(wordCount("http://paradigmzero.github.io/webpagewordcounter/index.html",[],[])).toEqual(20);
-    expect(wordCount("http://paradigmzero.github.io/webpagewordcounter/depth1embedded.html",[],[])).toEqual(120);
-    expect(wordCount("http://paradigmzero.github.io/webpagewordcounter/depth2embedded.html",[],[])).toEqual(140);
+  expect(
+    wordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/index.html",
+      [],
+      []
+    )
+  ).toEqual(20);
+  expect(
+    wordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/depth1embedded.html",
+      [],
+      []
+    )
+  ).toEqual(120);
+  expect(
+    wordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/depth2embedded.html",
+      [],
+      []
+    )
+  ).toEqual(140);
 });
 
-test(`dynamicWordCount method which will accurately count words on a page that changes the content with JavaScript.`, async ()=>{
-    // pages where JavaScript significantly changes the text
-    expect(await dynamicWordCount('https://paradigmzero.github.io/webpagewordcounter/scriptText.html')).toEqual(7);
-    expect(await dynamicWordCount('https://paradigmzero.github.io/webpagewordcounter/scriptTextComplex.html')).toEqual(20);
+test(`dynamicWordCount method which will accurately count words on a page that changes the content with JavaScript.`, async () => {
+  // pages where JavaScript significantly changes the text
+  expect(
+    await dynamicWordCount(
+      "https://paradigmzero.github.io/webpagewordcounter/scriptText.html"
+    )
+  ).toEqual(7);
+  expect(
+    await dynamicWordCount(
+      "https://paradigmzero.github.io/webpagewordcounter/scriptTextComplex.html"
+    )
+  ).toEqual(20);
 });
 
-test(`dynamicWordCount method with accurately count the text on a standard HTML page.`, async ()=>{
-    expect(await dynamicWordCount("http://paradigmzero.github.io/webpagewordcounter/index.html")).toEqual(20);
+test(`dynamicWordCount method with accurately count the text on a standard HTML page.`, async () => {
+  expect(
+    await dynamicWordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/index.html"
+    )
+  ).toEqual(20);
 });
 
-test(`dynamicWordCount method does not effectively count words with embedded HTML.`, async ()=>{
-    expect(await dynamicWordCount("http://paradigmzero.github.io/webpagewordcounter/depth1embedded.html")).not.toEqual(120);
-    expect(await dynamicWordCount("http://paradigmzero.github.io/webpagewordcounter/depth2embedded.html")).not.toEqual(140);
+test(`dynamicWordCount method does not effectively count words with embedded HTML.`, async () => {
+  expect(
+    await dynamicWordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/depth1embedded.html"
+    )
+  ).not.toEqual(120);
+  expect(
+    await dynamicWordCount(
+      "http://paradigmzero.github.io/webpagewordcounter/depth2embedded.html"
+    )
+  ).not.toEqual(140);
+});
+
+test("test to fail CI to check GitHub actions", async () => {
+  expect(false).toBe(true);
 });
